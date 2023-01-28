bdaa6d58f2e5600c7e92911a345cdaff94b6716b
mahdi <domanloom@gmail.com>
2023-01-28 19:40:22 +0330

    add notfication initial value to contextApi

--- react/src/context/ContextProvider.jsx
+++ react/src/context/ContextProvider.jsx
@@ -1,35 +1,41 @@
 import {createContext, useContext, useState} from "react";
 
 const StateContext = createContext({
-   user: null,
-   token:null,
-   setUser: ()=>{},
-   setToken: ()=>{},
+    user: null,
+    token: null,
+    notification: null,
+    setUser: () => {
+    },
+    setToken: () => {
+    },
 })
 
 export const ContextProvider = ({children}) => {
-   const [user, setUser] = useState({})
-   const [token , _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'))
-   const setToken = (token) => {
-      _setToken(token);
-      if (token){
-         localStorage.setItem('ACCESS_TOKEN' , token);
-      }else {
-         localStorage.removeItem('ACCESS_TOKEN')
-      }
-   }
+    const [user, setUser] = useState({})
+    const [notification, setNotification] = useState('')
+    const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'))
+    const setToken = (token) => {
+        _setToken(token);
+        if (token) {
+            localStorage.setItem('ACCESS_TOKEN', token);
+        } else {
+            localStorage.removeItem('ACCESS_TOKEN')
+        }
+    }
 
-   return (
-      <StateContext.Provider value={{
-         user,
-         token,
-         setUser,
-         setToken
-      }}>
-         {children}
-      </StateContext.Provider>
+    return (
+        <StateContext.Provider value={{
+            user,
+            token,
+            setUser,
+            setToken,
+            notification,
+            setNotification
+        }}>
+            {children}
+        </StateContext.Provider>
 
-   )
+    )
 }
 
 export const useStateContext = () => useContext(StateContext);
